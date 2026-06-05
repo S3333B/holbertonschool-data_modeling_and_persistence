51cb2888c672c6c47177c19147af64f627970ff3
Sébastien Lamblin <seb.lamblin59@gmail.com>
2026-06-05 16:23:03 +0200

    join

---
+++ sql_join/6-courses-above-average.sql
@@ -0,0 +1,14 @@
+SELECT courses.title AS course_title
+FROM courses
+INNER JOIN enrollments
+    ON courses.id = enrollments.course_id
+GROUP BY courses.id, courses.title
+HAVING COUNT(*) > (
+    SELECT AVG(enrollment_count)
+    FROM (
+        SELECT COUNT(*) AS enrollment_count
+        FROM enrollments
+        GROUP BY course_id
+    )
+)
+ORDER BY course_title ASC;
